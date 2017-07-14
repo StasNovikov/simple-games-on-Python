aa4adb806002a9de73920664f247e39187aa8ae5
StasNovikov <stasnovikov30@gmail.com>
2017-07-14 21:40:07 +0300

    add changes to the file no_vowels

--- no_vowels.py
+++ no_vowels.py
@@ -1,2 +1,11 @@
 # Только согласные
 # Демонстрирует, как создавать новые строки из исходных с помощью цикла for
+message = input("Введите текст: ")
+new_message = ""
+VOWELS = "aeiouaеёиоуыэюя"
+for letter in message:
+    if letter.lower() not in VOWELS:
+        new_message += letter
+        print("Создана новая строка: ", new_message)
+print("\nВот ваш текст с изятыми гласными буквами: ", new_message)
+print("\nPress the enter to exit")
