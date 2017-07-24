5678eae2d130650b8ca60a06d5ce050d6a44c03c
StasNovikov <stasnovikov30@gmail.com>
2017-07-24 08:17:51 +0300

    Work with try/except/else blocks.

--- handle_it.py
+++ handle_it.py
@@ -14,3 +14,32 @@ for value in (None, "Привет!"):
         print(float(value))
     except (TypeError, ValueError):
         print("Похоже, это не число!")
+
+# Другой вариант обработки исключений
+print()
+for value in (None, "Привет!"):
+    try:
+        print("Пытаюсь преобразовать в число: ", value, "-->", end=" ")
+        print(float(value))
+    except TypeError:
+        print("Я умею преобразовывать только строки и числа!")
+    except ValueError:
+        print("Я умею преобразовывать только строки, составленные из цифр!")
+
+# получение аргумента
+print()
+try:
+    num = float(input("\nВведите число: "))
+except ValueError as e:
+    print("Это не число!")
+    print(e)
+
+# try/except/else
+try:
+    num = float(input("\nВведите число: "))
+except ValueError:
+    print("Это не число!")
+else:
+    print("Вы введи число ", num)
+
+input("Press the enter key to exit!")
