44b2bf80ec2bbedc79e0663ddb7aa9d15e5fefbd
StasNovikov <stasnovikov30@gmail.com>
2017-07-30 14:06:19 +0300

    Changed the card module.

---
+++ OOP/Black-Jack/cards.py
@@ -0,0 +1,18 @@
+# Модуль cards
+# Набор базовых классов для карточной игры
+class Card():
+    """ Одна игральная карта """
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["c", "d", "h", "s"]
+
+    def __init__(self, rank, suit, face_up = True):
+        self.rank = rank
+        self.suit = suit
+        self.is_face_up = face_up
+
+    def __str__(self):
+        if self.is_face_up:
+            rep = self.rank + self.suit
+        else:
+            rep = "XX"
+        return rep
